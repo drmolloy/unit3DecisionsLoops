fd2b0fa2f191b64dd0f0e79e80b307c83a9ee914
drmolloy <drmolloy@student.naperville203.org>
2014-10-28 14:49:03 -0500

    Chapter 5 Practice Prompts

---
+++ CommonLoopAlgorithms/CompareAdjacent.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CompareAdjacent
+{
+    /**
+     * Detects duplicate adjacent numbers in the specified series of numbers.
+     */
+    public static void main( String[] args)
+    {
+        Scanner in = new Scanner( System.in );
+        System.out.println("Enter a series of numbers (type 'q' to quit)" );
+        
+        double prevValue = in.nextDouble();
+        
+        while( in.hasNextDouble())
+        {
+            double value = in.nextDouble();
+            if( value == prevValue )
+            {
+                System.out.println( "Duplicate number!");
+            }
+            
+            prevValue = value;
+        }
+    }
+}
